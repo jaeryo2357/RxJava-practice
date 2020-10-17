fd8c698a0ecf4300c412bd809b8f42f9a844d176
jaeryo2357 <jaeryo2357@naver.com>
2020-10-17 23:20:02 +0900

    combine and filter

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -3,5 +3,26 @@ import io.reactivex.rxjava3.core.Flowable;
 public class HelloWorld {
     public static void main(String[] args) {
         Flowable.just("Hello. RxJava").subscribe(System.out::println);
+
+        combine();
+
+        filter();
+    }
+
+    public static void combine() {
+        String[] data1 = new String[]{"1", "2", "3"};
+        String[] data2 = new String[]{"A", "B", "C"};
+
+        Flowable.combineLatest(
+                Flowable.fromArray(data1), Flowable.fromArray(data2),
+                (d1, d2) -> d1 + "-" + d2).subscribe(System.out::println);
+    }
+
+    public static void filter() {
+        Integer[] data = new Integer[] {1, 2, 3, 4, 5, 6};
+
+        Flowable.fromArray(data)
+                .filter(d -> d % 2 == 0)
+                .subscribe(System.out::println);
     }
 }
